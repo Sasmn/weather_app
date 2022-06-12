0f51de10cf312a53c9ac6cb04be5b8a33a57b9ca
sasmn <0.3sasi@gmail.com>
2022-06-12 17:59:02 +0200

    full page scroll beta

--- src/functions/fullPageScroll.js
+++ src/functions/fullPageScroll.js
@@ -1,3 +1,4 @@
+import { once } from "lodash";
 import domElements from "..";
 
 function fullPageScroll() {
@@ -14,213 +15,68 @@ function fullPageScroll() {
 
     /* ON PC */
     let scrollDirection = 'down';
-    let isScrolling = false;
 
-    /*
-        var timer = null;
-        window.addEventListener('wheel', function (e) {
-            e.preventDefault();
-            if (e.deltaY < 0) {
-                scrollDirection = 'down';
-            }
-            else if (e.deltaY > 0) {
-                scrollDirection = 'up';
-            }
-    
-            console.log(e.deltaY)
-    
-            if (timer !== null) {
-                clearTimeout(timer);
-            }
-    
-            timer = setTimeout(function () {
-                if (panels.length - 1 !== activePanel && scrollDirection == 'up') {
-                    activePanel++;
-                }
-                else if (activePanel !== 0 && scrollDirection == 'down') {
-                    activePanel--;
-                }
-                scrollPanels(activePanel);
-            }, 350);
-        }, { passive: false });
-    
-        window.addEventListener('scroll', () => {
-            console.log('help')
-        })
-    */
+    let y = 0;
+    let y2 = 0;
+    let y3 = 0;
+    let y4 = 0;
+    let asc = false;
 
-    /*
-    var timer = null;
-    window.addEventListener('wheel', function (e) {
-        e.preventDefault();
-        // isScrolling = true;
-
-        if (e.deltaY < 0) {
-            scrollDirection = 'down';
-        }
-        else if (e.deltaY > 0) {
-            scrollDirection = 'up';
-        }
-
-        console.log(e.deltaY)
-
-        if (timer !== null) {
-            clearTimeout(timer);
-        }
-
-        if (isScrolling == false) {
-            if (panels.length - 1 !== activePanel && scrollDirection == 'up') {
-                activePanel++;
-            }
-            else if (activePanel !== 0 && scrollDirection == 'down') {
-                activePanel--;
-            }
-            scrollPanels(activePanel);
-            isScrolling = true;
-            e.stopImmediatePropagation();
-        }
-
-        timer = setTimeout(function () {
-            isScrolling = false;
-        }, 350);
-    }, { passive: false });
-    */
-
-
-    /*
-    function detectTrackPad(e) {
-        var isTrackpad = false;
-        if (e.wheelDeltaY) {
-            if (Math.abs(e.wheelDeltaY) !== 120) {
-                isTrackpad = true;
-            }
-        }
-        else if (e.deltaMode === 0) {
-            isTrackpad = true;
-        }
-        console.log(isTrackpad ? "Trackpad detected" : "Mousewheel detected");
-    }
-
-    document.addEventListener("mousewheel", detectTrackPad, false);
-    document.addEventListener("DOMMouseScroll", detectTrackPad, false);
-*/
-    window.addEventListener("wheel", e => e.preventDefault(), { passive: false })
-
-    /*
-    window.addEventListener('scroll', (e) => {
-        window.scrollTo(0, 0)
-        console.log('BENNVAGYUNK')
-        console.log(window.pageYOffset)
-        e.preventDefault();
-    }, { passive: false })
-    */
-
-    var timer = null;
-    /*
-        for (let i = 0; i < panels.length; i++) {
-            panels[i].addEventListener('wheel', stop, { passive: false });
-            panels[i].addEventListener('wheel', (e) => {
-            });
-        }
-    
-        
-        function stop(e) {
-            e.preventDefault();
-    
-            if (e.deltaY < 0) {
-                scrollDirection = 'down';
-            }
-            else if (e.deltaY > 0) {
-                scrollDirection = 'up';
-            }
-    
-            if (isScrolling == false) {
-    
-                if (panels.length - 1 !== activePanel && scrollDirection == 'up') {
-                    activePanel++;
-                }
-                else if (activePanel !== 0 && scrollDirection == 'down') {
-                    activePanel--;
-                }
-                scrollPanels(activePanel);
-            }
-    
-            isScrolling = true;
-    
-            panels.forEach(p => {
-                p.removeEventListener('wheel', stop, { passive: false });
-                e.preventDefault();
-            });
-            /*
-            if (timer !== null) {
-                clearTimeout(timer);
-            }
-            *//*
-(e.wheelDelta > 0) ? -5 : 5;
-
-console.log(e);
-e.stopPropagation();
-setTimeout(function () {
-isScrolling = false;
-console.log(isScrolling)
-
-panels[activePanel].addEventListener('wheel', stop, { passive: false })
-
-// panels.forEach(p => {
-//     p.removeEventListener('wheel', stop, { passive: false });
-// });
-}, 850);
-
-}
-*/
-
-    let lefele = false;
-    let felfele = false;
-    let c = 0;
-
-    window.addEventListener('wheel', throttle(damnIt, 800))
+    window.addEventListener('wheel', throttle(damnIt, 300), { passive: false })
 
     function damnIt() {
-        console.log("hey");
-        console.log(event.deltaY)
-
-
-        console.log(c)
-        if (event.deltaY < 0) {
-            scrollDirection = 'down';
+        if (panels.length - 1 !== activePanel && scrollDirection == 'up') {
+            activePanel++;
         }
-        else if (event.deltaY > 0) {
-            scrollDirection = 'up';
+        else if (activePanel !== 0 && scrollDirection == 'down') {
+            activePanel--;
         }
-
-        if (isScrolling == false) {
-
-            if (panels.length - 1 !== activePanel && scrollDirection == 'up') {
-                activePanel++;
-            }
-            else if (activePanel !== 0 && scrollDirection == 'down') {
-                activePanel--;
-            }
-            scrollPanels(activePanel);
-        }
-        // window.addEventListener('wheel', throttle(damnIt, 2000), { once: true });
+        scrollPanels(activePanel);
     }
 
     function throttle(cb, timeout) {
-        console.log('eeeee')
+
+
         let lastCall = 0;
-        return function () {
+        return function (e) {
+            e.preventDefault()
 
+            if (Math.abs(event.deltaY) < 20) return;
 
-
-            if (Math.abs(event.deltaY) < 20 || Math.abs(event.deltaY) > 40) return;
-
-            if (new Date() - lastCall > timeout) {
-                console.log(new Date() - lastCall)
-                lastCall = new Date();
-                cb();
+            
+            if (e.deltaY - (y + y2 + y3 + y4) / 4 > 0) {
+                asc = true;
+            }
+            else {
+                asc = false;
             }
 
+            y4 = y3;
+            y3 = y2;
+            y2 = y;
+            y = e.deltaY;
+
+            
+            if (e.deltaY < 0) {
+                scrollDirection = 'down';
+            }
+            else if (e.deltaY > 0) {
+                scrollDirection = 'up';
+            }
+            
+
+            if (asc == false && scrollDirection == 'up') {
+                return;
+            }
+            else if (asc == true && scrollDirection == 'down') {
+                return;
+            }
+
+            if (new Date() - lastCall > timeout) {
+                lastCall = new Date();
+
+                cb();
+            }
         }
     }
 
@@ -260,4 +116,3 @@ function scrollPanels(activePanel) {
 }
 
 export default fullPageScroll;
-
