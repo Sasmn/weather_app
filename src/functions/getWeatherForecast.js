82eabef502db1613257de47a63c6608eba6f0a6e
sasmn <0.3sasi@gmail.com>
2022-06-16 19:41:22 +0200

    https again

--- src/functions/getWeatherForecast.js
+++ src/functions/getWeatherForecast.js
@@ -4,7 +4,7 @@ import getLocalDate from "./getLocalDate"
 
 export default async function getWeatherForecast(lat, lon, day) {
     /* fetch weather forecast */
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=82465ea8349914da9592b8c5629230a6&units=metric`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=82465ea8349914da9592b8c5629230a6&units=metric`, { mode: 'cors' });
     const forecastData = await response.json();
 
     /* remove the cards from the previous city */
